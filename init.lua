f719479b8fee16a1a089bd6270f813d05e25c488
Eric Shull <eric@exupero.org>
2026-07-16 12:33:09 -0400

    Update Fennel path

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-package.path = package.path .. ";/opt/homebrew/Cellar/fennel/1.5.1/share/lua/5.1/?.lua"
+package.path = package.path .. ";/opt/homebrew/Cellar/fennel/1.6.1_1/share/lua/5.5/?.lua"
 
 fennel = require('fennel')
 fennel.path = package.path .. ";" .. os.getenv("HOME") .. "/code/dotfiles/hammerspoon/?.fnl;" .. os.getenv("HOME") .. "/code/hammerspoon/?.fnl;"
